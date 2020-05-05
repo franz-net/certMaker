f44c8b2da6553f20fb35302bcdce1b3ae5a99896
milic001 <5334409+franz-net@users.noreply.github.com>
2020-05-04 21:45:21 -0400

    separated functions into more atomic funtions

--- certMaker.go
+++ certMaker.go
@@ -20,9 +20,49 @@ import (
 	"time"
 )
 
-func genCA() (*rsa.PrivateKey, []byte, *x509.Certificate) {
-	// Generates CA cert
-	cadef := &x509.Certificate{
+func genPrivateKey() *rsa.PrivateKey {
+	privateKey, err := rsa.GenerateKey(rand.Reader, 4096)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	return privateKey
+}
+
+func writeCertKey(key *bytes.Buffer, cert *bytes.Buffer, certType string) {
+	certPath := ""
+	keyPath := ""
+
+	if certType == "ca" {
+		certPath = "./caCert.pem"
+		keyPath = "./caKey.pem"
+	} else {
+		certPath = "./sCert.pem"
+		keyPath = "./sKey.pem"
+	}
+
+	_ = ioutil.WriteFile(certPath, key.Bytes(), 0775)
+	_ = ioutil.WriteFile(keyPath, cert.Bytes(), 0775)
+}
+
+func encodeCertKey(key *rsa.PrivateKey, cert []byte) (*bytes.Buffer, *bytes.Buffer) {
+	pemCert := new(bytes.Buffer)
+	pem.Encode(pemCert, &pem.Block{
+		Type:  "CERTIFICATE",
+		Bytes: cert,
+	})
+
+	pemKey := new(bytes.Buffer)
+	pem.Encode(pemKey, &pem.Block{
+		Type:  "RSA PRIVATE KEY",
+		Bytes: x509.MarshalPKCS1PrivateKey(key),
+	})
+
+	return pemCert, pemKey
+}
+func genCaCertDef() *x509.Certificate {
+	// Generates x509 certificate
+	caDef := &x509.Certificate{
 		SerialNumber: big.NewInt(2020),
 		Subject: pkix.Name{
 			Organization:  []string{"Example, Inc."},
@@ -40,36 +80,22 @@ func genCA() (*rsa.PrivateKey, []byte, *x509.Certificate) {
 		BasicConstraintsValid: true,
 	}
 
-	caKeyBin, err := rsa.GenerateKey(rand.Reader, 4096)
-	if err != nil {
-		panic(err.Error())
-	}
-
-	caCertBin, err := x509.CreateCertificate(rand.Reader, cadef, cadef, &caKeyBin.PublicKey, caKeyBin)
-	if err != nil {
-		panic(err.Error())
-	}
-
-	caCert := new(bytes.Buffer)
-	pem.Encode(caCert, &pem.Block{
-		Type:  "CERTIFICATE",
-		Bytes: caCertBin,
-	})
-
-	caKey := new(bytes.Buffer)
-	pem.Encode(caKey, &pem.Block{
-		Type:  "RSA PRIVATE KEY",
-		Bytes: x509.MarshalPKCS1PrivateKey(caKeyBin),
-	})
-
-	_ = ioutil.WriteFile("./caKey.pem", caKey.Bytes(), 0775)
-	_ = ioutil.WriteFile("./caCert.pem", caCert.Bytes(), 0775)
-
-	return caKeyBin, caCertBin, cadef
+	return caDef
 }
 
-func genSCert() (*rsa.PrivateKey, *x509.Certificate) {
-	// Device Cert information
+func genCA(caKeyBin *rsa.PrivateKey, caDef *x509.Certificate) []byte {
+	// Generates CA cert binary
+
+	caCertBin, err := x509.CreateCertificate(rand.Reader, caDef, caDef, &caKeyBin.PublicKey, caKeyBin)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	return caCertBin
+}
+
+func genSCertDef() *x509.Certificate {
+	// Generates x509 certificate
 	sCertDef := &x509.Certificate{
 		SerialNumber: big.NewInt(1658),
 		Subject: pkix.Name{
@@ -89,45 +115,44 @@ func genSCert() (*rsa.PrivateKey, *x509.Certificate) {
 		KeyUsage:     x509.KeyUsageDigitalSignature,
 	}
 
-	sKeyBin, err := rsa.GenerateKey(rand.Reader, 4096)
-	if err != nil {
-		panic(err.Error())
-	}
-
-	return sKeyBin, sCertDef
+	return sCertDef
 }
 
 func signCert(caKey *rsa.PrivateKey, caCert []byte, sKeyBin *rsa.PrivateKey, sCertDef *x509.Certificate, caCertDef *x509.Certificate) []byte {
+
 	sCertBin, err := x509.CreateCertificate(rand.Reader, sCertDef, caCertDef, &caKey.PublicKey, caKey)
 	if err != nil {
 		panic(err.Error())
 	}
 
-	sCert := new(bytes.Buffer)
-	pem.Encode(sCert, &pem.Block{
-		Type:  "CERTIFICATE",
-		Bytes: sCertBin,
-	})
-
-	sKey := new(bytes.Buffer)
-	pem.Encode(sKey, &pem.Block{
-		Type:  "RSA PRIVATE KEY",
-		Bytes: x509.MarshalPKCS1PrivateKey(sKeyBin),
-	})
-
-	_ = ioutil.WriteFile("./sKey.pem", sKey.Bytes(), 0775)
-	_ = ioutil.WriteFile("./sCert.pem", sCert.Bytes(), 0775)
-
 	return sCertBin
 }
 
 func main() {
-	caKey, caCert, caCertDef := genCA()
-	sKeyBin, sCertDef := genSCert()
-	sCert := signCert(caKey, caCert, sKeyBin, sCertDef, caCertDef)
-	if sCert == nil {
-		fmt.Println("Error...")
-	}
+	fmt.Println("Generating CA...")
+	// generate ca key
+	caKeyBin := genPrivateKey()
+	// generate cert definition
+	caCertDef := genCaCertDef()
+	// generate ca cert and return it
+	caCertBin := genCA(caKeyBin, caCertDef)
+	// pem encode the cacert and return it
+	caPemCert, caPemKey := encodeCertKey(caKeyBin, caCertBin)
+	// Write the cert to file
+	writeCertKey(caPemKey, caPemCert, "ca")
+
+	fmt.Println("Generating signed Cert...")
+	// generate server cert key
+	sKeyBin := genPrivateKey()
+	// generate cert definition
+	sCertDef := genSCertDef()
+	// Sign cert with CA
+	sCertBin := signCert(caKeyBin, caCertBin, sKeyBin, sCertDef, caCertDef)
+	// pem encode the scert and return it
+	sPemCert, sPemKey := encodeCertKey(sKeyBin, sCertBin)
+	// write the cert to file
+	writeCertKey(sPemKey, sPemCert, "")
+
 	fmt.Println("Done!")
 
 }
